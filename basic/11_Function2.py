b389a5e08e50b317ecc47815e67ad6167a7c7341
opennote <poptome@opennote.co.kr>
2025-02-03 14:19:52 +0900

    Python Basic Study

--- basic/11_Function2.py
+++ basic/11_Function2.py
@@ -1,3 +1,131 @@
 # 함수 (Function)
-# ---- 학습목표 ----
+print();print()
+print("=== 두개이상의 매개변수가 있는 함수" , "="* 60)
+#  ■ 매개변수는 함수가 필요로 하는 만큼 정의할수있다. (최대 255개)
+#  ■ 일반적으로 함수 호출시 매개변수의 개수만큼 값을 입력해준다.
+#  ■ 매개변수가 여러개일때 호출시 입력된 값이 차례대로 저장된다.
+#  ex) def fnArguments(a, b, c) :
+#           함수내부수행로직1
+#           함수내부수행로직2
+#
+#      fnArguments(10, 20, 30)
+#
+#      fnArguments의 A는 10, B는 20, C는 30이 대입된다.
+
+def fnHap(a, b) :
+        result = a + b
+        print('두수의 합을 구해 출력해주는 함수입니다.')
+        print(a,',', b, '두수의 합은 ',result,'입니다.')
+
+fnHap(10, 20)
+fnHap(-87, 172)
+
+
+print("=== 함수의 return 값" , "="* 60)
+#  ■ return문이 없는 함수는 None값을 반환한다.
+#  ■ return문은 하나의 객체만 반환할수 있다.
+#  ■ 리스트, 튜플, 사전등과 같은 군잡자료형을 사용하면 한번에 여러개의 값을 반환할수있다.
+
+def fnTurnNone(value) :
+    x = value
+
+def fnTurnValue(value) :
+    x = value * 10
+    return x
+
+def fnTurnDict(value) :
+    x = {value, value+1 , value +2} # 군집자료형인 Dictionary(사전)로 반환
+    return x
+
+def fnTurnDictKv(value) :
+    x = {'value':value, "value1":value+1 , "value2":value +2} # 군집자료형인 Dictionary(사전)로 반환
+    return x
+
+def fnTurnSet(value) :
+    return value, value-1, value-2
+
+print('fnTurnNone : ', fnTurnNone(10) )
+print('fnTurnValue : ', fnTurnValue(100) )
+print('------')
+rtnDict = fnTurnDict(10)
+print('fnTurnDict:', rtnDict)
+print('fnTurnDict:', rtnDict , "rtnDict.pop():",rtnDict.pop()) # Key가 없으면 pop로 값을 찾는다. 그후 값은 소멸된다.(우측 함수먼저수행되어 10이 없어졌다.)
+print('------')
+rtnDictKv = fnTurnDictKv(10) #
+print("rtnDictKv[value]:",rtnDictKv["value"]
+    , ", rtnDictKv[value1]:",rtnDictKv["value1"]
+    , ", rtnDictKv[value2]:",rtnDictKv["value2"])
+print('------')
+rtnSet = fnTurnSet(10)
+print( "fnTurnSet:", rtnSet
+     , ", rtnSet[0]:",rtnSet[0]
+     , ", rtnSet[1]:",rtnSet[1]
+     , ", rtnSet[2]:",rtnSet[2])
+
+print("=== 매개변수의 기본값 설정하기" , "="* 60)
+#  ■ 매개변수에 기본값을 설정할수있다.
+#  ■ 기본값이 설정된 매개변수는 함수 호출시 값 지정을 생략하면 기본값이 할당된다.
+#  ■ 기본값이 설정된 매개변수는 반드시 기본값이 없는 매개변수 뒤에만 배치할수 있다.
+#  ex)  func(message, who='Everyone')              # 가능
+#       func(message, who='Everyone', when='am')   # 가능
+#       func(who='Everyone', message, when='am')   # 불가능 (기본값이 없는 매개변수 앞에 기본값이 있는 매개변수가 위치)
+#       func(who='Everyone', when='am', message)   # 불가능 (기본값이 없는 매개변수 앞에 기본값이 있는 매개변수가 위치)
+
+def report(message, who='EveryOne') :
+    print(message , who)
+
+report('Good Morning')  # 기본값적용
+report('Good Morning', 'Mr. Kim')
+
+print("=== 가변인수" , "="* 60)
+#  ■ 함수에 필요한 값이 여러개이면서 가변적일떄는 가변인수를 사용한다.
+#  ■ 가변인수는 함수의 매개변수에 '*'를 붙여 지정한다.
+#  ■ 가변인수는 한 함수에 하나만 사용할수있다.
+#  ■ 가변인수뒤에 매개변수는 올수없다.
+#   ex)  def func(*arg)          # 가능
+#        def func(a, *arg)       # 가능
+#        def func(a, b, *arg)    # 가능
+#        def func(*arg1, *arg2)  # 오류발생(가변인수는 한 함수에 하나만 사용할수있다.)
+#        def func(*arg1, c)      # 오류발생(가변인수뒤에 매개변수는 올수없다.)
+
+def select_even(*arg) :
+    result = []
+    for num in arg :
+        if num%2 == 1:   # 나머기자 1이면 홀수
+            continue
+        result.append(num)
+    return result
+
+print(select_even(1,2,3,4))
+print(select_even(-12,2,81,99,48,20))
+
+
+print("=== 함수정보 표시하기(__doc__)" , "="* 60) # '__'는 Double UnderScore라고 한다. 통칭 약어로 던더(DunDer)라고 칭한다.
+#  ■ 함수에대한 정보를 얻고 싶을떄 내장함수인 help()함수를 하용해서 함수의 정보를 파악할수있다.
+#  ■ 함수 설명문은 여러 쥴 문자열 지정('''~''')을 이용해야 하며, 반드시 내부에서 사용되는 명령어 앞에 작성되어야 한다.
+#  ■ 이미 지정된 함수 설명문을 수정하고 싶다면 __doc__ 속성을 이용해 재정의 할수있다.
+
+def fnPrint_odd(start, end) :
+    '''주어진 범위에서 홀수를 리스트로 만들어 주는 함수입니다.
+    :param start: 시작값을 지정함니다. (정수형, int)
+    :param end: 종료값을 지정합니다. (정수형, int)
+    :return: list(홀수값들)
+    '''
+    result = []
+    for num in range(start, end+1) :
+        if num % 2 == 0 :
+            continue
+        result.append(num)
+
+    return result
+
+print("함수의 doc문서 파악", "-"*30)
+help(fnPrint_odd) # 함수의 doc문서 파악
+print("-"*30)
+fnPrint_odd.__doc__ = '''이건 연습삼아서 재정의Test'''
+help(fnPrint_odd) # 함수의 doc문서 파악
+print("-"*30)
+print(fnPrint_odd(3,19))
+
+
 
---
+++ basic/12_PythonScope.py
@@ -0,0 +1,86 @@
+# 변수의 범위
+# --- 학습목표 ---
+#  ■ 전역변수와 지역변수의 차이점을 설명할수있다.
+#  ■ 동일한 이름의 변수가 전역에 모두 있을때 변수의 우선적용 범위를 설명할수있다.
+#  ■ 'global' 명령어의 역할을 설명 할수있다.
+
+print("=== 전역변수와 지역변수" , "="* 60)
+#  - 변수는 유효범위(Scope)에 따라 전역변수와 지역변수로 구분한다.
+#  - 전역변수와 지역변수를 구분짓는 위치는 변수가 선언된 위치이다.
+#    1) 전역변수
+#       ● 프로그램 전체 역역(전역)에 사용할수있는 변수
+#       ● 블록(함수블록 또는 클래스 블록) 밖에서 변수를 만들어야 한다.
+#       ● 전역변수는 프로그램 실행 내내 사용할 수 있다.
+#    2) 지역변수
+#       ● 한정된 영역(지역)에서는 사용할수있는 변수
+#       ● 함수블록 또는 클래스블록 내부에서 변수를 만들어야 한다.
+#       ● 지역변수는 블록내 변수가 만들어 진 지점에서 생성,소멸된다.
+#       ● 지역변수는 해당 지역변수가 만들어 진 함수 블록이나 클래스 블록 밖에서 사용할수없다.
+#       ● 함수의 매개변수도 지역변수에 해당된다.
+
+global_let = '전역변수'
+
+def fnScopePrint() :
+    local_let = '지역변수'
+    print('fnScope내부 global_let : ', global_let)
+    print('fnScope내부 local_let : ',local_let)
+
+fnScopePrint()
+print("--"*20)
+print('fnScope외부 global_let : ', global_let)
+#print('fnScope외부 local_let : ',local_let)  # NameError: name 'local_let' is not defined (함수내부에서 선언된 변수는 참조 불가능)
+
+print();print()
+print("=== 동일한 이름의 변수가 전역,지역에 있을떄" , "="* 60)
+#  - 전역과 지역에 동일한 이름의 변수가 있어도 이 둘은 전혀 다른 변수이다.
+#  - 동일한 이름의 변수가 전역과 지역에 모두 있을떄 변수 사용규칙
+#    1) 먼저 해당 변수가 사용된 지역 내에서 동일한 이름의 변수를 찾는다.
+#    2) 자신이 사용된 지역에서 동일한 이름의 변수가 없다면, 상위의 더 넓은 영역(상위지역 또는 전역)에서 그이름의 변수를 찾는다.
+
+letScope = '전역변수'
+def fnScope():
+    letScope = '지역변수' #  변수명이 같아도 ,값을 덮어 쓰지는 않음
+    print(letScope)
+
+fnScope()  # 지역변수
+print('--')
+print(letScope) # 전역변수
+
+print();print()
+print("=== global 선언" , "="* 60)
+#  - 지역에서 만들어 지는 변수를 전역 변수로 만들고자 할때 global 명령어를 사용 (지역변수를 전역변수로 만들떄)
+#  - 전역변수의 유효범위는 프로그램 전체 이지만
+#    지역 변수는 해당 지역 변수가 선언된 함수블록 이나 클래스블록 내부로 한정하기 때문에
+#    함수블록이나 클래스블록 내부에서 선언된 변수에 저장된 내용을
+#    함수나 클래스 외부에서 계속 사용하기 위해서 global 명령어가 사용된다.
+
+const = "전역변수"
+def fnGrobalScope():
+    global const                             # 지역변수를 전역변수로 만들며, 값을 덮어 쓴다.
+    const = 'Global 명령어의 역할'
+    print('fnGrobalScope 함수 안 const : ',const)
+
+fnGrobalScope()
+print()
+print('함수밖 const : ', const);  # let는 'global' 키워드를 사용해서 전역변수 Scope로변경,
+
+print();print()
+print("=== 변수의 범위" , "="* 60)
+#  - 새로운 지역변수가 만들어 질수있는 영역은 함수내부, 클래스내부, 모듈내부 이다.
+#  - if, for, while, with , try ~ except 블록 등은 새로운 변수를 만들지 못한다.
+
+genLet = '전역변수'
+def fnGenLetScope() :
+    genLet = 'scope() 지역변수'
+    print(genLet , '\t[fnGenLetScope 함수에서 출력]')  # 'scope() 지역변수'
+
+    for i in range(3) :
+        genLet = 'for 지역변수'
+        print(genLet , '\t[fnGenLetScope for문에서 출력]') # 'for 지역변수'
+
+    if True :
+        print(genLet , '\t[fnGenLetScope if문에서 출력]')  # 'for 지역변수'
+
+
+fnGenLetScope()
+print(genLet, '\t\t[ scope() 함수 밖에서 출력]')
